f149ecc8402f8e376fefadb0a1f33a9e12c2712f
Stefan Bilharz <stefan@sbsoftware.de>
2013-11-20 22:37:09 +0100

    Introduce addDir() and corresponging tests

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 (function () {
   'use strict';
 
+  var fs = require('fs');
+
   function Modules() {
   }
 
   Modules.prototype.add = function (name, type, module) {
     this[name] = [type, module];
+  };
+
+  Modules.prototype.addDir = function (dirPath) {
+    var that = this;
+    var files = fs.readdirSync(dirPath);
+
+    files.filter(function (file) {
+      return file.indexOf('.js') > 0;
+    }).forEach(function (file) {
+      file = file.replace('.js', '');
+      that.add(file, 'factory', require(dirPath + '/' + file));
+    });
   };
 
   module.exports = Modules;
--- test/spec.js
+++ test/spec.js
@@ -2,6 +2,10 @@
   'use strict';
 
   var should = require('should');
+  var sinon = require('sinon');
+  var expectRequire = require('a').expectRequire;
+
+  var fs = require('fs');
   var Modules = require('../index.js');
 
   describe('di-modules', function () {
@@ -11,6 +15,7 @@
         Object.keys(modules).should.have.length(0);
       });
     });
+
     describe('methods', function () {
       var modules;
 
@@ -25,6 +30,39 @@
           modules.testModule.should.be.an.Array;
           modules.testModule[0].should.equal('factory');
           modules.testModule[1].should.equal(testModule);
+        });
+      });
+
+      describe('.addDir()', function () {
+        var readdirStub;
+        var module1, module2;
+
+        beforeEach(function () {
+          readdirStub = sinon.stub(fs, 'readdirSync');
+          readdirStub.returns(['.', '..', 'module1.js', 'wrong.html', 'module2.js']);
+          module1 = {};
+          module2 = {};
+          expectRequire('./testDir/module1').return(module1);
+          expectRequire('./testDir/module2').return(module2);
+        });
+
+        it('should require every .js file in the given folder', function () {
+          // requiring ., .. or wrong.html would throw "Cannot find module" error
+          modules.addDir('./testDir');
+        });
+
+        it('should add the required modules as type "factory" to the object', function () {
+          modules.addDir('./testDir');
+          modules.module1.should.be.an.Array;
+          modules.module1[0].should.equal('factory');
+          modules.module1[1].should.equal(module1);
+          modules.module2.should.be.an.Array;
+          modules.module2[0].should.equal('factory');
+          modules.module2[1].should.equal(module2);
+        });
+
+        afterEach(function () {
+          fs.readdirSync.restore();
         });
       });
     });
